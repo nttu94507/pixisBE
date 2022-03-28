686863a5b1fc13f586878ff40f04b90e7c377fd9
nttu94507 <nttu94507@gmail.com>
2022-03-28 15:48:59 +0800

    init done

---
+++ database/migrations/2022_02_15_080653_probe_info.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('probes', function (Blueprint $table) {
+            $table->id();
+            $table->string('probeid');
+            $table->string('harddiskdrive');
+            $table->string('status');
+            $table->string('owner');
+            $table->string('shipid')->nullable();
+            $table->timestamp('ship_at')->nullable();
+            $table->string('Purchaseid');
+            // $table->timestamp('Purchase_at');
+            $table->timestamps();
+        });
+        //
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
