640ada94e73a2f17682264eebc9a25e5d4bdd314
yuchih.fu <nttu94507@gmail.com>
2023-07-18 18:47:57 +0800

    [M]store Customer

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -23,11 +23,11 @@ class CustomerController extends Controller
         $currentTime = Carbon::now();
         $data = new Customer();
         $data->GUInumber = $request->GUInumber;
-        $data->Organization_Name = $request->OrganizationName;
-        $data->Organization_Address = $request->OrganizationAddress;
-        $data->contactPerson = $request->contactPerson;
-        $data->contactPerson_Email = $request->contactPersonEmail;
-        $data->contactPerson_PhoneNumber = $request->contactPersonPhoneNumber;
+        $data->Organization_Name = $request->Organization_Name;
+        $data->Organization_Address = $request->Organization_Address;
+        $data->contactPerson = $request->contractPerson;
+        $data->contactPerson_Email = $request->contractPerson_Email;
+        $data->contactPerson_PhoneNumber = $request->contractPerson_Phonenumber;
         $data->status = 0;
         $data->FAEID = $request->FAEID;
         $data->SalesID = $request->SalesID;
